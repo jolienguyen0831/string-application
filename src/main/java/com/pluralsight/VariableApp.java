86a128aadf8c17b837d682326e9e5d646cdbb366
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-09 13:19:23 -0500

    feat: display step 1

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+
+        //Step 1
+        String favoriteColor = "blue";
+        System.out.println("My favorite color is: " + favoriteColor);
+        short classYear = 2026;
+        System.out.println("My class year at YUU is: " + classYear);
+        String middleInitial = "NP";
+        System.out.println("My middle initial is: " + middleInitial);
+        boolean havePets = false;
+        System.out.println("Have pets? " + havePets);
+        String niceMessage = "Beautiful day!";
+        System.out.println("Message: " + niceMessage);
+    }
 }
