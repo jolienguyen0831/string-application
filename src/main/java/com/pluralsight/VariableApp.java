be43cd652c62b9248681835db270fe45e817647c
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-09 13:24:01 -0500

    feat: display step 2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -14,5 +14,18 @@ public class VariableApp {
         System.out.println("Have pets? " + havePets);
         String niceMessage = "Beautiful day!";
         System.out.println("Message: " + niceMessage);
+
+        //Step 2
+        byte numberDaysInAWeek = 7;
+        System.out.println("Number of days in a week is: " + numberDaysInAWeek);
+        float priceOfCoffee = 4.99f;
+        System.out.println("Price of coffee is: " + priceOfCoffee);
+        char myFavoriteLetter = 'U';
+        System.out.println("My favorite letter is: " + myFavoriteLetter);
+        boolean isRaining = false;
+        System.out.println("Is raining? " + isRaining);
+
+
+
     }
 }
